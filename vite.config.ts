79c799680576aa81e03b3e759ca18e763dc7b7a3
Zack Jones <zackjones11@gmail.com>
2024-01-24 17:32:54 +0100

    Added import alias (~/*)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     include: ['./**/**/*.test.ts', './**/**/*.test.tsx'],
-  }
+  },
+  resolve: {
+    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
+  },
 })
